ff0c8b19d6f0d65a4dea5e1d0b79dd627854ce2e
Athar Syed <athar.syed@pizzahutaustralia.com.au>
2025-09-13 16:45:43 +1000

    migrate from sqlexpress to sqlite

--- src/MathTutor.Api/Program.cs
+++ src/MathTutor.Api/Program.cs
@@ -7,21 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options =>
-    // /* options.UseSqlite("Data Source=mathtutor.db")); */
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), 
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"), 
         b => b.MigrationsAssembly("MathTutor.Infrastructure")));
-
-/* builder.Services.ConfigureHttpJsonOptions(options =>
-{
-    options.SerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
-    options.SerializerOptions.PropertyNameCaseInsensitive = true;
-});
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi(); */
 
 builder.Services.AddScoped<ISessionService, SessionService>();
 
@@ -32,23 +19,6 @@ builder.Services.AddControllers()
     });
 
 var app = builder.Build();
-
-/* // Apply migrations automatically
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    await db.Database.MigrateAsync();
-}
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-// API Endpoints
-app.MapSessionEndpoints(); */
 
 app.MapControllers();
 
